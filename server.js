5f639f3e72d3d7b4794c17fa62c0306e8486c61d
Mario Olivares <mario@linkapital.com.br>
2022-04-23 23:15:01 -0400

    test1

--- server.js
+++ server.js
@@ -24,9 +24,56 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
+app.get("/api/:date?", function (req, res) {
+  var moment = require('moment'); // require
+//"Fri, 25 Dec 2015 00:00:00 GMT"
+  try{
+    let response = null;
+    const dateParam = req.params.date;
+
+    // empty catch
+    if(undefined == dateParam || '' == dateParam) {
+      response = {
+        unix: parseInt(moment.utc().format('x')),
+        utc: moment.utc()
+      }
+    }
+    else {
+      if(isNaN(dateParam)) {
+        if( moment(dateParam).isValid()) {
+          let t = moment.utc(dateParam).format('x');
+          t = parseInt(t);
+          response = {
+            unix: t,
+            utc: moment.utc(dateParam).format("ddd, DD MMM YYYY") + ' 00:00:00 GMT'
+          }
+        }else {
+          throw new Error('Invalid Date');
+        }
+      }else {
+        if(dateParam.toString().length == 13) {
+          response = {
+            unix: parseInt(dateParam),//+'000'
+            utc: moment.utc(parseInt(dateParam)).format("ddd, DD MMM YYYY") + ' 00:00:00 GMT'
+          }
+        }else {
+          throw new Error('Invalid Date');
+        }
+      }
+    }
+    
+    res.json(response);
+
+ }catch(err){
+   res.json({
+     error: err.message
+   })
+ }
+  
+});
 
 
 // listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
+var listener = app.listen(3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
